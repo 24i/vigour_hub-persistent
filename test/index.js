3c8ef41830f00dc3cb1faea7065899fae53e6ef0
Mustafa Dokumaci <mustafa@foraa.com>
2017-02-02 15:36:16 +0100

    load from riak

--- test/index.js
+++ test/index.js
@@ -33,61 +33,15 @@ test('dirty check', t => {
     })
 
   setTimeout(() => {
-    client.set(null)
-    dataHub.set(null)
-    t.end()
-  }, 3e3)
-
-  /*
-  setTimeout(() => {
-    dataHub.set({
-      persistent: {
-        server: {
-          port: 9091,
-          host: 'localhost'
-        }
-      }
-    })
-  }, 2000)
-
-  setTimeout(() => {
-    server.close()
-  }, 3000)
-
-  setTimeout(() => {
-    dataHub.set({
-      persistent: {
-        server: {
-          port: 9090,
-          host: 'localhost'
-        }
-      }
-    })
-  }, 4000)
-
-  setTimeout(() => {
-    server = pServer(9090)
-
-    client.set({
-      someData: { to: 'test' },
-      someOther: 'data',
-      andAnother: { pathOne: 2, pathTwo: 1 }
-    })
-  }, 5000)
-
-  setTimeout(() => {
     dataHub.get('persistent').load(false)
       .then(() => {
-        console.log('load promise resolved')
+        t.pass('load promise resolved')
         client.set(null)
         dataHub.set(null)
-        server.close()
         t.end()
       })
       .catch(error => {
         console.log('failed loading', error)
       })
-  }, 6000)
-
-  */
+  }, 1e3)
 })
